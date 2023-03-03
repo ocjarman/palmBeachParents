faa8b9865af3d885a3a913e7f4d087889a9315be
ocjarman <ocjarman@gmail.com>
2023-03-02 20:54:58 -0500

    made resource model

--- server/api/events.ts
+++ server/api/events.ts
@@ -1,14 +1,15 @@
 import express, { Request, Response, NextFunction } from "express";
-import { User } from "db";
+import { User, Event } from "../db/index";
 const router = express.Router();
 
 // api/events
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const header = req.headers.authorization;
-    const token = header && header.split(" ")[1];
-    if (!token) return res.status(404).send("No Token Found");
-    res.send('events page');
+    // const header = req.headers.authorization;
+    // const token = header && header.split(" ")[1];
+    // if (!token) return res.status(404).send("No Token Found");
+    let events = await Event.findAll()
+    res.send(events);
   } catch (err) {
     res.sendStatus(404);
     next(err);
