f71b0035b57a4a00be9ae4239b734b88ac1d1d89
ocjarman <ocjarman@gmail.com>
2023-03-07 11:03:41 -0500

    profile update user info css toggling & in correct location

--- src/components/views/Profile/Profile.tsx
+++ src/components/views/Profile/Profile.tsx
@@ -2,42 +2,77 @@ import React from "react";
 import { RootState } from "../../../store";
 import { useSelector } from "react-redux";
 import { Container } from "@mui/system";
-import Button from '../../CustomMUI/Button'
-import './profile.css'
+import Button from "../../CustomMUI/Button";
+import Typography from "../../CustomMUI/Typography";
+import "./profile.css";
 import UpdateUserInfoForm from "./UpdateUserInfoForm";
 import { setShowUpdateForm } from "../../../store/userSlice";
 import { useDispatch } from "react-redux";
 
 const Profile = () => {
-  const  user = useSelector((state: RootState) => state.user.user);
-  const showUpdateForm  = useSelector((state: RootState) => state.user.showUpdateForm);
-  const dispatch = useDispatch()
+  const user = useSelector((state: RootState) => state.user.user);
+  const showUpdateForm = useSelector(
+    (state: RootState) => state.user.showUpdateForm
+  );
+  const dispatch = useDispatch();
 
   return (
-    <div className="profilePage">
-      <h1 className="profileHeader">{user.username}'s Profile</h1>
-      <div className="userInfo">
-        <img src={`${user.avatarUrl}`} alt="profile picture" className="profilePicture"></img>
-        <p>Username: {user.username}</p>
-        <p>Name: {user.fullName}</p>
-        <p>Account Type: {user.accountType}</p>
-      </div>
-      <div className="userContactInfo">
-        <p>Phone number: {user.phoneNum}</p>
-        <p>E-mail Address: {user.email}</p>
-        <p>Birthday: {user.birthday}</p>
-        <p>Mailing Address: {user.address}</p>
-        <p>Company Name: {user.companyName ? `${user.companyName}` : 'not listed'}</p>
-           <Button
-            size="small"
-            onClick={() => dispatch(setShowUpdateForm(true))}
-            variant="contained"
-            sx={{ my: 2, color: "secondary.light", display: "block" , maxWidth: '25vw'}}
-            >Edit Personal Info
+    <> 
+    {!showUpdateForm && (
+        <div className="profilePage">
+          <Typography className="profileHeader" variant={"h3"}>
+            {user.username}'s Profile
+          </Typography>
+          <div className="userInfo">
+          <img
+            src={`${user.avatarUrl}`}
+            alt="profile picture"
+            className="profilePicture"
+          ></img>
+            <Typography className="profileHeader" variant={"body1"}>
+              Username: {user.username}
+            </Typography>
+            <Typography className="profileHeader" variant={"body1"}>
+              Name: {user.fullName}
+            </Typography>
+            <Typography className="profileHeader" variant={"body1"}>
+              Account Type: {user.accountType}
+            </Typography>
+          </div>
+          <div className="userContactInfo">
+            <Typography className="profileHeader" variant={"body1"}>
+              Phone number: {user.phoneNum}
+            </Typography>
+            <Typography className="profileHeader" variant={"body1"}>
+              E-mail Address: {user.email}
+            </Typography>
+            <Typography className="profileHeader" variant={"body1"}>
+              Birthday: {user.birthday}
+            </Typography>
+            <Typography className="profileHeader" variant={"body1"}>
+              Mailing Address: {user.address}
+            </Typography>
+            <Typography className="profileHeader" variant={"body1"}>
+              Company Name: {user.companyName ? user.companyName : "not listed"}
+            </Typography>
+            <Button
+              size="small"
+              onClick={() => dispatch(setShowUpdateForm(true))}
+              variant="contained"
+              sx={{
+                my: 2,
+                color: "secondary.light",
+                display: "block",
+                maxWidth: "25vw",
+              }}
+            >
+              Edit Personal Info
             </Button>
-      </div>
-      {showUpdateForm && <UpdateUserInfoForm/>}
-    </div>
+          </div>
+          </div>
+      )}
+      {showUpdateForm && <UpdateUserInfoForm />}
+    </>
   );
 };
 
