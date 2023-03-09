26724a84c030588ed8d093a11b830ee35c3eaa39
ocjarman <ocjarman@gmail.com>
2023-03-09 14:19:27 -0500

    working on margins on add event form

--- src/components/views/Admin/NewEventForm.tsx
+++ src/components/views/Admin/NewEventForm.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Button, Container, Typography, TextField, Paper } from "@mui/material";
+import { Container } from "@mui/material";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { RootState } from "../../../store";
 import { setEvents, setNewEvent } from "../../../store/eventsSlice";
 import MenuItem from "@mui/material/MenuItem";
-import {Input, FormControl, InputLabel} from "@mui/material";
+import { Input, FormControl, InputLabel } from "@mui/material";
+import Button from "../../CustomMUI/Button";
+import Typography from "../../CustomMUI/Typography";
+import TextField from "../../CustomMUI/TextField";
+
 const NewEventForm = () => {
   const newEvent = useSelector((state: RootState) => state.events.newEvent);
   const dispatch = useDispatch();
@@ -26,7 +30,10 @@ const NewEventForm = () => {
   }) => {
     try {
       event.preventDefault();
-      const { data: created } = await axios.post("/api/events/addEvent", newEvent);
+      const { data: created } = await axios.post(
+        "/api/events/addEvent",
+        newEvent
+      );
       dispatch(setEvents(created));
       dispatch(setNewEvent({}));
       navAllEvents();
@@ -59,203 +66,201 @@ const NewEventForm = () => {
   ];
 
   return (
-    <Paper>
+    <Container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        placeSelf: "center",
+        gap: "20px",
+        padding: "20px",
+      }}
+    >
+      <Typography sx={{ placeSelf: "center" }} variant={"h5"}>
+        Add New Event
+      </Typography>
+
+      <form
+        style={{
+          gap: "20px",
+          alignItems: "center",
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <Container
+          sx={{
+            margin: '1%',
+            gap: "20px",
+          }}
+        >
+          <TextField
+            required
+            id="name"
+            name="name"
+            label="Event Title"
+            onChange={handleNewEvent}
+          />
+          <TextField
+            required
+            id="address"
+            name="address"
+            label="Event Address"
+            onChange={handleNewEvent}
+          />
+          <FormControl required>
+            <InputLabel shrink htmlFor="birthday-input">
+              Date
+            </InputLabel>
+            <Input
+              type="date"
+              name="date"
+              id="date-input"
+              aria-describedby="date-helper-text"
+              onChange={handleNewEvent}
+            />
+          </FormControl>
+          <TextField
+            required
+            id="time"
+            name="time"
+            label="Time"
+            onChange={handleNewEvent}
+          />
+        </Container>
+         <Container
+          sx={{
+            margin: '1%',
+            gap: "20px",
+          }}
+        >
+          <TextField
+            required
+            id="description"
+            name="description"
+            label="description"
+            onChange={handleNewEvent}
+          />
+          <TextField
+            required
+            id="url"
+            name="url"
+            label="Link to Event"
+            onChange={handleNewEvent}
+          />
+          <TextField
+            required
+            id="hostName"
+            name="hostName"
+            label="Host Name Or Company"
+            onChange={handleNewEvent}
+          />
+          <TextField
+            required
+            id="hostPhone"
+            name="hostPhone"
+            label="Host Phone"
+            onChange={handleNewEvent}
+          />
+          <TextField
+            required
+            id="hostEmail"
+            name="hostEmail"
+            label="Host Email"
+            onChange={handleNewEvent}
+          />
+        </Container>
+         <Container
+          sx={{
+            margin: '1%',
+            gap: "20px",
+          }}
+        >
+          <TextField
+            required
+            id="price"
+            name="price"
+            label="Price"
+            type="number"
+            onChange={handleNewEvent}
+          />
+          <TextField
+            required
+            id="imageUrl"
+            name="imageUrl"
+            label="Image Url"
+            onChange={handleNewEvent}
+          />
+          <TextField
+            id="select-recurring"
+            select
+            label="Recurring Event"
+            // SelectProps={{
+            //   native: true,
+            // }}
+            name="recurring"
+            helperText="Is this a recurring event?"
+            onChange={handleNewEvent}
+            defaultValue=""
+          >
+            <MenuItem value={"yes"}>Yes</MenuItem>
+            <MenuItem value={"no"}>No</MenuItem>
+          </TextField>
+          <TextField
+            id="select-category"
+            select
+            label="Category"
+            defaultValue=""
+            // SelectProps={{
+            //   native: true,
+            // }}
+            helperText="What type of event is this?"
+            name="category"
+            onChange={handleNewEvent}
+          >
+            {categories.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField
+            required
+            id="age"
+            name="age"
+            label="Ages"
+            onChange={handleNewEvent}
+          />
+        </Container>
+      </form>
       <Container
         sx={{
           display: "flex",
-          flexDirection: "column",
-          placeSelf: "center",
           gap: "20px",
-          padding: "20px",
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
-        <Typography sx={{ placeSelf: "center" }} variant={"h5"}>
-          Add New Event
-        </Typography>
-
-        <Container
-          sx={{
-            display: "flex",
-            gap: "20px",
-            justifyContent: "center",
-            alignItems: "center",
-            placeSelf: "center",
-          }}
+        <Button
+          color="secondary"
+          variant="contained"
+          size="small"
+          component="button"
+          onClick={handleSubmitNewEvent}
         >
-          <form
-            style={{
-              display: "flex",
-              gap: "20px",
-              flexDirection: "column",
-              width: "80%",
-              justifyContent: "center",
-              alignItems: "center",
-              placeSelf: "center",
-            }}
-          >
-            <TextField
-              required
-              id="name"
-              name="name"
-              label="Event Title"
-              variant="outlined"
-              onChange={handleNewEvent}
-            />
-            <TextField
-              required
-              id="address"
-              name="address"
-              label="Event Address"
-              variant="outlined"
-              onChange={handleNewEvent}
-            />
-           <FormControl required>
-              <InputLabel shrink htmlFor="birthday-input">
-                Date
-              </InputLabel>
-              <Input
-                type="date"
-                name="date"
-                id="date-input"
-                aria-describedby="date-helper-text"
-                onChange={handleNewEvent}
-                
-              />
-              
-            </FormControl>
-            <TextField
-              required
-              id="time"
-              name="time"
-              label="Time"
-              variant="outlined"
-              onChange={handleNewEvent}
-            />
-            <TextField
-              required
-              id="description"
-              name="description"
-              label="description"
-              variant="outlined"
-              onChange={handleNewEvent}
-            />
-            <TextField
-              required
-              id="url"
-              name="url"
-              label="Link to Event"
-              variant="outlined"
-              onChange={handleNewEvent}
-            />
-            <TextField
-              required
-              id="hostName"
-              name="hostName"
-              label="Host Name Or Company"
-              variant="outlined"
-              onChange={handleNewEvent}
-            />
-            <TextField
-              required
-              id="hostPhone"
-              name="hostPhone"
-              label="Host Phone"
-              variant="outlined"
-              onChange={handleNewEvent}
-            />
-            <TextField
-              required
-              id="hostEmail"
-              name="hostEmail"
-              label="Host Email"
-              variant="outlined"
-              onChange={handleNewEvent}
-            />
-            <TextField
-              required
-              id="price"
-              name="price"
-              label="Price"
-              variant="outlined"
-              type="number"
-              onChange={handleNewEvent}
-            />
-            <TextField
-              required
-              id="imageUrl"
-              name="imageUrl"
-              label="Image Url"
-              variant="outlined"
-              onChange={handleNewEvent}
-            />
-            <TextField
-              id="select-recurring"
-              select
-              label="Recurring Event"
-              // SelectProps={{
-              //   native: true,
-              // }}
-              name="recurring"
-              helperText="Is this a recurring event?"
-              variant="outlined"
-              onChange={handleNewEvent}
-              defaultValue=''
-            >
-              <MenuItem value={'yes'}>
-                  Yes
-                </MenuItem>
-              <MenuItem value={'no'}>
-                  No
-                </MenuItem>
-
-            </TextField>
-            <TextField
-              id="select-category"
-              select
-              label="Category"
-              defaultValue=''
-              // SelectProps={{
-              //   native: true,
-              // }}
-              helperText="What type of event is this?"
-              name="category"
-              variant="outlined"
-              onChange={handleNewEvent}
-            >
-              {categories.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </TextField>
-
-            <TextField
-              required
-              id="age"
-              name="age"
-              label="Ages"
-              variant="outlined"
-              onChange={handleNewEvent}
-            />
-          </form>
-        </Container>
-        <Container
-          sx={{
-            display: "flex",
-            gap: "20px",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
+          Add Event
+        </Button>
+        <Button
+          color="secondary"
+          variant="contained"
+          size="small"
+          component="button"
+          onClick={navAllEvents}
         >
-          <Button variant="contained" onClick={handleSubmitNewEvent}>
-            Add Event
-          </Button>
-          <Button variant="contained" onClick={navAllEvents}>
-            Cancel
-          </Button>
-        </Container>
+          Cancel
+        </Button>
       </Container>
-    </Paper>
+    </Container>
   );
 };
 
