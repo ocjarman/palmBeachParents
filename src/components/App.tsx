7f7b0854bce3f091dbb929b8d91ba72834291834
ocjarman <ocjarman@gmail.com>
2023-03-11 20:15:40 -0500

    clean up before PR

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,53 +1,41 @@
-import React, { useEffect } from 'react';
-import { setUser } from '../store/userSlice';
-import { useSelector, useDispatch } from 'react-redux';
-import axios from 'axios';
-import { RootState } from '../store';
-import RouterComponent from './RouterComponent';
-import ResponsiveAppBar from './Navbar/ResponsiveAppBar';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from '../theme';
-import Footer from './Footer/Footer'
-import GetEvents from '../utils/Custom Hooks/GetEvents';
-import { Outlet } from 'react-router-dom';
-import PageHero from './PageHero/PageHero';
+import React, { useEffect } from "react";
+import { setUser } from "../store/userSlice";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import ResponsiveAppBar from "./Navbar/ResponsiveAppBar";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../theme";
+import GetEvents from "../utils/Custom Hooks/GetEvents";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
-    const { user } = useSelector((state: RootState) => state.user);
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const loginWithToken = async () => {
-        const token = window.localStorage.getItem('token');
-        if (token) {
-            const response = await axios.get('/api/auth', {
-                headers: {
-                    authorization: token
-                }
-            });
-            dispatch(setUser(response.data))
-        }
-    };
+  const loginWithToken = async () => {
+    const token = window.localStorage.getItem("token");
+    if (token) {
+      const response = await axios.get("/api/auth", {
+        headers: {
+          authorization: token,
+        },
+      });
+      dispatch(setUser(response.data));
+    }
+  };
 
-    useEffect(() => {
-        loginWithToken();
-    }, []);
+  useEffect(() => {
+    loginWithToken();
+  }, []);
 
-// custom hooks
- GetEvents()
+  // custom hooks
+  GetEvents();
 
-    return (
-        <ThemeProvider theme={theme}>
-            <div>
-                {/* NAV BAR */}
-                <ResponsiveAppBar/>
-                {/* ROUTER COMPONENT */}
-                {/* <RouterComponent/> */}
-                <Outlet/>
-                {/* FOOTER */}
-                {/* <Footer/> */}
-            </div>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <ResponsiveAppBar />
+      <Outlet />
+    </ThemeProvider>
+  );
 };
 
 export default App;
