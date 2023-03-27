deebe73a9ce1fc18de4d4d1c0ba97fdf21841d6a
ocjarman <ocjarman@gmail.com>
2023-03-27 13:22:06 -0400

    deleted unused model

--- server/db/index.ts
+++ server/db/index.ts
@@ -3,23 +3,15 @@ import User from './models/User';
 import Event from './models/Event';
 import Address from './models/Address';
 import Favorite from './models/Favorite';
-import UserFavorites from './models/UserFavorites';
+// import UserFavorites from './models/UserFavorites';
 
 // make associations here
 User.belongsToMany(Event, {through: 'users_events'} )
 User.hasMany(Favorite)
-// User.belongsToMany(Favorite, {through: 'user_favorites'})
-// Favorite.belongsToMany(User, {through: 'user_favorites'})
-Favorite.belongsToMany(User, {through: UserFavorites})
 
 
 User.hasOne(Address)
 Event.hasOne(Address)
 Favorite.hasOne(Address)
-
-// Address.belongsTo(User)
-// Address.belongsTo(Event)
-// Address.belongsTo(Favorite)
-
 
 export { User, Event, Address, db, Favorite };
